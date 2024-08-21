4fd43ba8b71e3e5e495afc66bca2394aa15ad343
Serhii Berdianskyi <zeferot94@gmail.com>
2024-08-21 10:59:35 +0300

    created Redactor, Topic and Newspaper models

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.CharField(max_length=255)
+
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="redactor_groups",
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="redactor_user_permissions",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=False, null=False)
+    published_date = models.DateTimeField(auto_now_add=True)
+    topics = models.ManyToManyField(Topic, related_name="newspapers")
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self):
+        return self.title
