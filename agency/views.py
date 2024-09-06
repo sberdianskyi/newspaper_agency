3cb8c92763f7d700d9fe3fc3959d79e74b6e53b0
Serhii Berdianskyi <zeferot94@gmail.com>
2024-09-06 10:46:25 +0300

    created index function based view, TopicListView, NewspaperListView and RedactorListView

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from agency.models import Redactor, Newspaper, Topic
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "agency/index.html", context=context)
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    queryset = Topic.objects.all().order_by("name")
+    paginate_by = 5
+    template_name = "agency/topic_list.html"
+    context_object_name = "topic_list"
+
+
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
+    model = Newspaper
+    queryset = Newspaper.objects.prefetch_related("topics")
+    paginate_by = 5
+    template_name = "agency/newspaper_list.html"
+    context_object_name = "newspaper_list"
+
+
+class RedactorListView(LoginRequiredMixin, generic.ListView):
+    model = Redactor
+    paginate_by = 5
+    template_name = "agency/redactor_list.html"
+    context_object_name = "redactor_list"
