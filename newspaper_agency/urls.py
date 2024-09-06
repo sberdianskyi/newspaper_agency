588604dda843c5529647322dd8aec91719e18eb4
Serhii Berdianskyi <zeferot94@gmail.com>
2024-09-06 11:17:46 +0300

    added static in newspaper_agency/urls.py

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("agency.urls", namespace="agency")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
