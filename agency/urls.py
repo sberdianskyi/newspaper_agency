ebf0eb0fa564d06b9120ee698d360d5fcb9aff98
Serhii Berdianskyi <zeferot94@gmail.com>
2024-09-06 10:53:37 +0300

    created urls for home page, topics, newspapers and redactors list views

---
+++ agency/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from agency.views import index, TopicListView, NewspaperListView, RedactorListView
+
+urlpatterns = [
+    path("", index, name="index"),
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topic-list"
+    ),
+    path(
+        "newspapers/",
+        NewspaperListView.as_view(),
+        name="newspaper-list"
+    ),
+    path(
+        "redactors/",
+        RedactorListView.as_view(),
+        name="redactor-list"
+    ),
+]
+
+app_name = "agency"
