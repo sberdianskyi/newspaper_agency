93ab341259cb2d6534318e0e2696c49ec0038fad
Serhii Berdianskyi <zeferot94@gmail.com>
2024-09-10 10:36:39 +0300

    added accounts path

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -23,4 +23,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("agency.urls", namespace="agency")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
