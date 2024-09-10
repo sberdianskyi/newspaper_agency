e69fe37818ea0fdf653eeba40f91161b259dc207
Serhii Berdianskyi <zeferot94@gmail.com>
2024-09-10 11:08:57 +0300

    added path for create, update and delete Topic views

--- agency/urls.py
+++ agency/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from agency.views import index, TopicListView, NewspaperListView, RedactorListView
+from agency.views import (index,
+                          TopicListView,
+                          TopicCreateView,
+                          TopicUpdateView,
+                          TopicDeleteView,
+                          NewspaperListView,
+                          RedactorListView, )
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,6 +14,21 @@ urlpatterns = [
         "topics/",
         TopicListView.as_view(),
         name="topic-list"
+    ),
+    path(
+        "topics/create/",
+        TopicCreateView.as_view(),
+        name="topic-create"
+    ),
+    path(
+        "topics/<int:pk>/update/",
+        TopicUpdateView.as_view(),
+        name="topic-update"
+    ),
+    path(
+        "topics/<int:pk>/delete/",
+        TopicDeleteView.as_view(),
+        name="topic-delete"
     ),
     path(
         "newspapers/",
